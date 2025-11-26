d141c4a97d79ce0e9a9dfbb4b72c5f8b8dae73aa
Ethan Jed Carbonell <131255754+Ethanolbicarbonate@users.noreply.github.com>
2025-11-26 10:20:01 +0800

    security fixes

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -1,12 +1,9 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseConfig {
-  // Phase 1 Credentials
-  // Replace with your actual Project URL
-  static const String url = 'https://yltepchbwwrsavfgbhzx.supabase.co';
-  
-  // Replace with your actual Anon Key
-  static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlsdGVwY2hid3dyc2F2ZmdiaHp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4MjAxMjEsImV4cCI6MjA3OTM5NjEyMX0.28YDeB_bzmc2M6YWwDrdzWuuHoBcpd54vUwUMO5yYl0';
+  static String get url => dotenv.env['SUPABASE_URL'] ?? '';
+  static String get anonKey => dotenv.env['SUPABASE_ANON_KEY'] ?? '';
 
   static Future<void> initialize() async {
     await Supabase.initialize(
